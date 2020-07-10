7b558446093ff21d42271c134de53d124c56c79e
serge-mur <serge-mur@yandex.ru>
2020-07-10 16:03:19 +0300

    verstka

--- js/script.js
+++ js/script.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
         dots: true
     });
 
+    $('.card-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true
+    });
+
+    $('.utp-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true
+    });
+
 });
