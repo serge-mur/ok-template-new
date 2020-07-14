53acfad96a230b2251ab4ae90beee2a6b36cbba7
serge-mur <serge-mur@yandex.ru>
2020-07-14 17:00:52 +0300

    verstka

--- js/script.js
+++ js/script.js
@@ -1,51 +1,365 @@
 $(document).ready(function() {
+    
+    // табы на главной
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        console.log(e.target.id);
+        if (e.target.id == 'goods-tab') {
+            $('#tab-2-brands').hide();
+            $('#tab-2-goods').show();
+        } else {
+            $('#tab-2-goods').hide();            
+            $('#tab-2-brands').show();
+        }
+    })
 
     $('.main-slider').slick({
         dots: true,
         arrows: false
     });
-    setArrows('.main-slider');
-
   
-    $('.card-slider').slick({
-        infinite: true,
+    // Товары которые вы смотрели и покупали
+    $('#slider-product-1.card-slider').slick({
+        infinite: false,
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
     });
-    setArrows('.card-slider');
-
-    $('.card-slider-3col').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false
-    });
-    setArrows('.card-slider-3col');
-
 
     $('.utp-slider').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        dots: false
+        dots: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
     });
 
-    // $('.collection-slider').slick({
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     dots: true
-    // });
+    // Новинки
+    $('#slider-product-2.card-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
 
-    // $('.recipes-slider').slick({
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     dots: true
-    // });
+    // Товары со скидками
+    $('#slider-product-3.card-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    // подборки
+    $('.collection-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    // рецепты
+    $('.recipes-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    // Выбор покупателей
+    $('#slider-product-4.card-slider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    // Молочные продукты
+    $('#slider-product-5.card-slider').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    // статьи
+    $('.article-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    variableWidth: true
+                }
+            },
+        ]
+    });
+
+    if ($(window).width() > 575) {
+        setArrows('.main-slider');
+        setArrows('#slider-product-1.card-slider');
+        setArrows('#slider-product-2.card-slider');
+        setArrows('#slider-product-3.card-slider');
+        setArrows('.collection-slider');
+        setArrows('.recipes-slider');
+        setArrows('#slider-product-4.card-slider');
+        setArrows('#slider-product-5.card-slider');
+        setArrows('.article-slider');
+    }
 
 
 
